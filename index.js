453efe4c6f89ccbd8ce118e31b9bcbf6bc738d88
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-04-02 17:01:04 +0200

    render

---
+++ index.js
@@ -0,0 +1,15 @@
+const about = (
+  <div>
+    <img src="./react-logo.png" width="40px" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook/Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(about, document.getElementById("root"));
