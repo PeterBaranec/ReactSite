98b1e9f7bd3b6093bc5637ff113604d892573f94
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-04-02 17:35:52 +0200

    organize components

---
+++ Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>© 2024 Peter Baranec. All rights reserved.</small>
+    </footer>
+  );
+}
--- index.js
+++ index.js
@@ -1,40 +1,6 @@
-function Header() {
-  return (
-    <header>
-      <nav className="nav">
-        <img src="./react-logo.png" className="nav-logo" />
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function MainContent() {
-  return (
-    <div>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook/Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2024 Peter Baranec. All rights reserved.</small>
-    </footer>
-  );
-}
+import Header from "./Header";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
 
 function Page() {
   return (
